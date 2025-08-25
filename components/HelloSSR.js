d1b85d486b7ca9d68a3dc08ef8f712a97015d027
Lalit Rana <lalitrana2018r@gmail.com>
2025-08-25 14:36:09 +0530

    he

--- components/HelloSSR.js
+++ components/HelloSSR.js
@@ -1,7 +1,7 @@
 export default function HelloSSR({ message }) {
   return (
     <div style={{ padding: "20px", background: "#f3f3f3", marginTop: "20px" }}>
-      <h2>Hello from SSR!</h2>
+      <h2>Hello from SSR! Hello working </h2>
       <p>{message}</p>
     </div>
   );
